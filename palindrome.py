d61cca14a9e07f200aa77528be7028e17831ae13
KSu2 <44729913+KSu2@users.noreply.github.com>
2020-06-16 10:49:53 -0400

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,34 @@
+#your name
+#the date
+#Palindrome Project
+
+x = 5
+y = "hi"
+z = True
+
+#your comment
+
+print(x)
+print(y)
+print(z)
+
+
+#your comment
+
+if(z):
+    print("z equals true")
+if(x < 6):
+    print("x is less than 6")
+if(y == "bye"):
+    print("y equals 'bye'")
+else:
+    print("y doesn't equal 'bye'")
+
+#your comment
+
+a = ''.join(reversed("hi"))
+b = input()
+print(a)
+print(b)
+
+#your comment
